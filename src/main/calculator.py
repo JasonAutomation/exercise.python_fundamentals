a39098601063d0b485d8b2cf3e8a428bd7e263a6
dragonjay <j753k@hotmail.com>
2020-12-03 13:16:47 -0500

    add

--- src/main/calculator.py
+++ src/main/calculator.py
@@ -1,6 +1,8 @@
 # Created by Leon Hunter at 9:54 AM 10/23/2020
 class Calculator(object):
     def add(self, a, b):
+        return a + b
+
         return None # TODO - Implement solution
 
     def subtract(self, a, b):
