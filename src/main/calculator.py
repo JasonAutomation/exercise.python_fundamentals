8efd545ac85722b52d12ba84bebf5cbdd446d5bd
dragonjay <j753k@hotmail.com>
2020-12-03 14:36:17 -0500

    fix division2

--- src/main/calculator.py
+++ src/main/calculator.py
@@ -3,14 +3,14 @@
 
 class Calculator(object):
     def add(self, a, b):
-        return (a + b)
+        return a+b
 
     def subtract(self, a, b):
-        return (a-b)
+        return a-b
 
     def multiply(self, a, b):
-        return (a * b)
+        return a*b
 
     def divide(self, a, b):
 
-        return (a / b)
+        return round(a/b,3)
